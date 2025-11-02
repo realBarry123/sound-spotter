590dca946976cd93798ec9189a75695985cf0e73
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-11-01 23:27:36 -0400

    Fix training code

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import os, random, math
+import os, random
 import torch
 from tqdm import tqdm
 from model import SoundSpotter
@@ -11,7 +11,7 @@ B = 1
 SAMPLE_RATE = 44100
 DEVICE = "cpu"
 
-NUM_EPOCHS = 1
+NUM_EPOCHS = 4
 
 TEST_SIZE = 5
 
@@ -38,13 +38,17 @@ for epoch in range(NUM_EPOCHS):
     print(f"Epoch {epoch}/{NUM_EPOCHS}")
     for i in tqdm(range(train_size), desc=f"Training epoch {epoch}"):
         short_category = list(categories)[i]
-        y = math.round(random.triangular(0, 10, 3))
-        x = create_random_long(random.triangular(0, 10, 3), short_category)
+        y = round(random.triangular(0, 5, 2))
+        x = create_random_long(int(y), short_category)
 
-        short = torch.load(random.choice(os.listdir("dataset_pt_files/" + short_category)))
+        short = torch.load("dataset_pt_files/" + short_category + "/" + random.choice(os.listdir("dataset_pt_files/" + short_category)))
+
         short = add_noise(short)
+        short = short.unsqueeze(0)
 
         y_pred = model(x, short)
+        y = torch.tensor([[y]]).float()
+
         loss = mse_loss(y, y_pred)
 
         optimizer.zero_grad()
@@ -57,14 +61,18 @@ for epoch in range(NUM_EPOCHS):
 
     model.eval()
     total_val_loss = 0
-    for i in range(TEST_SIZE, len(list(categories)), desc=f"Testing epoch {epoch}"):
+    for i in tqdm(range(train_size, len(list(categories))), desc=f"Testing epoch {epoch}"):
         short_category = list(categories)[i]
-        y = math.round(random.triangular(0, 10, 3))
-        x = create_random_long(random.triangular(0, 10, 3), short_category)
-        short = torch.load(random.choice(os.listdir("dataset_pt_files/" + short_category)))
+        y = round(random.triangular(0, 5, 2))
+        x = create_random_long(int(y), short_category)
+        short = torch.load("dataset_pt_files/" + short_category + "/" + random.choice(os.listdir("dataset_pt_files/" + short_category)))
         short = add_noise(short)
+        short = short.unsqueeze(0)
         y_pred = model(x, short)
+        y = torch.tensor([[y]]).float()
         total_val_loss = mse_loss(y, y_pred).item()
 
     print(f"Average validation loss: {total_val_loss / TEST_SIZE}")
-    
+
+    if SAVING:
+        torch.save([model.state_dict(), epoch], SAVE_PATH)
