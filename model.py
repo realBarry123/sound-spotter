b50f33bade3cbaebee77ecd765310ecbcd6d9811
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2025-11-01 20:28:50 -0400

    Clean up model

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 F = 128
-B = 1
+B = 4
 
 class SoundSpotter(nn.Module):
     def __init__(self):
@@ -38,42 +38,30 @@ class SoundSpotter(nn.Module):
     def to_query(self, short):
         # (B, 1, F, T)
         short = self.short_conv1(short)
-        short = self.short_conv2(short)
-        # (B, 16, F, T)
-        short = short.mean(dim=-1, keepdim=False)
-        # (B, 16, F)
-        short = short.permute(0, 2, 1)
-        # (B, F, T=16)
-        # short = short.squeeze(0)
+        short = self.short_conv2(short) # (B, 16, F, T)
+        short = short.mean(dim=-1, keepdim=False) # (B, 16, F)
+        short = short.permute(0, 2, 1) # (B, F, T=16)
         return short
-    
-    def conv_heatmap(self, x, query):
-        heatmaps = []
-        for i in range(B):
-            kernel = query[i].unsqueeze(0)
-            heatmap = torch.nn.functional.conv1d(x[i:i+1], kernel)
-            heatmaps.append(heatmap)
-        heatmaps = torch.cat(heatmaps, dim=0)
-        return heatmaps
 
     def forward(self, x, short):
-        # (B x 1 x F x T)
+        # (B, 1, F, T)
         x = self.long_conv1(x)
         x = self.long_conv2(x)
-        print("x: " + str(x.shape))
 
         query = self.to_query(short)
-        print("query: " + str(query.shape))
-        x = x.squeeze(1)
-        print("x: " + str(x.shape))
+        x = x.squeeze(1) # (B, F, T)
+
+        heatmap_list = []
+        for i in range(B):
+            # x[i]: (F, T), query[i]: (F, K)
+            conv_out = torch.nn.functional.conv1d(
+                x[i:i+1], weight=query[i:i+1]
+            )
+            heatmap_list.append(conv_out)
         
-        # (B x 1 x F x T)
-        # heatmap = torch.nn.functional.conv2d(x, query)
-        heatmap = self.conv_heatmap(x, query)
-        
-        # (B x 1 x T)
-        count = heatmap.squeeze(1).sum(dim=-1, keepdim=False)
-        # (B x 1)
+        # (B, 1, T')
+        heatmap = torch.cat(heatmap_list, dim=0)
+        count = heatmap.squeeze(1).sum(dim=-1, keepdim=False) # (B)
         return count
 
 def test_model_shape():
